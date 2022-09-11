64addef0ee362c538020423aa225a08bbf7365b2
LDJWJ <frontier1020@naver.com>
2022-09-11 17:31:41 +0900

    20220911_update
    20220911_update

--- 02_02_programming_contact.py
+++ 02_02_programming_contact.py
@@ -1,3 +1,6 @@
+# 연락처 프로그램 만들기
+
+import time
 
 class Contact:
     def __init__(self, name, phone_number, e_mail, addr):
@@ -11,14 +14,9 @@ class Contact:
         print("Phone Number: ", self.phone_number)
         print("E-mail: ", self.e_mail)
         print("Address: ", self.addr)
-        menu = input("메뉴선택: ")
-        return int(menu)
+        print("")
 
-## 출력해 보기
-def print_contact(contact_list):
-    for contact in contact_list:
-        contact.print_info()
-
+# 메뉴 출력 
 def print_menu():
     print("1. 연락처 입력")
     print("2. 연락처 출력")
@@ -27,6 +25,7 @@ def print_menu():
     menu = input("메뉴선택: ")
     return int(menu)
 
+# 연락처 입력 함수
 def set_contact():
     name = input("Name: ")
     phone_number = input("Phone Number: ")
@@ -35,6 +34,14 @@ def set_contact():
     contact = Contact(name, phone_number, e_mail, addr)
     return contact
 
+# 연락처 출력
+def print_contact(contact_list):
+    len_num = len(contact_list)
+    print(f"{len_num}개의 연락처")
+    for contact in contact_list:
+        contact.print_info()
+
+# 연락처 삭제
 def delete_contact(contact_list, name):
     for i, contact in enumerate(contact_list):
         if contact.name == name:
@@ -42,18 +49,29 @@ def delete_contact(contact_list, name):
 
 def run():
     contact_list = []
-    while 1:
+
+    menu = print_menu()
+    print("메뉴 선택 :", menu)
+    
+    while (menu!=4):
         menu = print_menu()
         if menu == 1:
-            contact = set_contact()
-            contact_list.append(contact)
+            c1 = set_contact()
+            contact_list.append(c1)
+            print("연락처 저장 완료")
+            print(f"{len(contact_list)}개의 연락처 있습니다.")
+            
         elif menu == 2:
             print_contact(contact_list)
+            
         elif menu == 3:
             name = input("Name: ")
             delete_contact(contact_list, name)
+            
         elif menu == 4:
             break
+
+    time.sleep(5)  # 실행 완료 후, 지연 5초
 
 if __name__ == "__main__":
     run()
